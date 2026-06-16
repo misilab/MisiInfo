72a9e124ffd8181880171391fd7df76985ab0132
Misiraca <contact@misiraca.com>
2026-06-16 15:59:04 +0200

    Initial Commit

---
+++ MediaScope/MediaScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaScopeApp.swift
+//  MediaScope
+//
+//  Created by Matthieu Misiraca on 16/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MediaScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
